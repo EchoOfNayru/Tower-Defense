6afe2b4a3d992a13e524f1ced8d149159ad730e6
Cameron Zach P Tucker <s179071@students.aie.edu.au>
2018-04-10 11:57:57 -0700

    added first iteration of towers

--- Assets/Scripts/MouseInteraction.cs
+++ Assets/Scripts/MouseInteraction.cs
@@ -6,19 +6,25 @@ using UnityEngine.UI;
 public class MouseInteraction : MonoBehaviour {
 
     public GameObject wall;
+    public GameObject tower;
     public Button wallButton;
     public Button destroyButton;
+    public Button towerButton;
 
     bool wallSelected;
     bool destroySelected;
+    bool towerSelected;
 
     BasicEnemy enemyClicked;
     OceanNodeScript oceanNodeClicked;
     WallScript wallClicked;
+    TowerScript towerClicked;
 
     // Use this for initialization
     void Start() {
         wallSelected = false;
+        destroySelected = false;
+        towerSelected = false;
     }
 
     // Update is called once per frame
@@ -40,18 +46,26 @@ public class MouseInteraction : MonoBehaviour {
                         Destroy(enemyClicked.gameObject);
                     }
 
-                    //create wall on click
+                    //create structure on click
                     oceanNodeClicked = hit.collider.GetComponent<OceanNodeScript>();
                     if (oceanNodeClicked != null && oceanNodeClicked.isBuilt == false)
                     {
+                        //wall
                         if (wallSelected)
                         {
                             Instantiate(wall, hit.collider.transform);
                             oceanNodeClicked.isBuilt = true;
                         }
+                        //tower
+                        if (towerSelected)
+                        {
+                            Instantiate(tower, hit.collider.transform);
+                            oceanNodeClicked.isBuilt = true;
+                        }
                     }
 
                     //destroy structure on click
+                    //wall
                     wallClicked = hit.collider.GetComponent<WallScript>();
                     if (wallClicked != null)
                     {
@@ -62,14 +76,33 @@ public class MouseInteraction : MonoBehaviour {
                             Destroy(wallClicked.gameObject);
                         }
                     }
+                    //tower
+                    towerClicked = hit.collider.GetComponent<TowerScript>();
+                    if (towerClicked != null)
+                    {
+                        if (destroySelected)
+                        {
+                            oceanNodeClicked = towerClicked.gameObject.GetComponentInParent<OceanNodeScript>();
+                            oceanNodeClicked.isBuilt = false;
+                            Destroy(towerClicked.gameObject);
+                        }
+                    }
                 }
             }
         }
 
+        //keyboard shortcuts
+        //select wall
         if (Input.GetKeyDown("q"))
         {
             SetBuilderToWall();
         }
+        //select tower
+        if (Input.GetKeyDown("w"))
+        {
+            SetBuilderToTower();
+        }
+        //select destroy
         if (Input.GetKeyDown("d"))
         {
             SetBuilderToDestroy();
@@ -84,6 +117,8 @@ public class MouseInteraction : MonoBehaviour {
         wallButton.interactable = false;
         destroySelected = false;
         destroyButton.interactable = true;
+        towerSelected = false;
+        towerButton.interactable = true;
     }
 
     public void SetBuilderToDestroy()
@@ -92,5 +127,17 @@ public class MouseInteraction : MonoBehaviour {
         destroyButton.interactable = false;
         wallSelected = false;
         wallButton.interactable = true;
+        towerSelected = false;
+        towerButton.interactable = true;
+    }
+
+    public void SetBuilderToTower()
+    {
+        towerSelected = true;
+        towerButton.interactable = false;
+        wallSelected = false;
+        wallButton.interactable = true;
+        destroySelected = false;
+        destroyButton.interactable = true;
     }
 }
---
+++ Assets/Scripts/TowerScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerScript : MonoBehaviour {
+
+    public GameObject bullet;
+    public float shootInterval;
+    public float damage;
+    public float bulletSpeed;
+
+    GameObject currentTarget;
+    BasicEnemy targetEnemy;
+    float timer;
+
+    void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= shootInterval)
+        {
+            ShootAtEnemy(currentTarget);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        targetEnemy = other.GetComponent<BasicEnemy>();
+        if (targetEnemy != null)
+        {
+            timer = shootInterval;
+            currentTarget = targetEnemy.gameObject;
+        }
+    }
+
+    void ShootAtEnemy(GameObject target)
+    {
+        GameObject tempBullet = Instantiate(bullet);
+        Rigidbody tempRB = tempBullet.GetComponent<Rigidbody>();
+        Vector3 targetVector3 = target.transform.position;
+        tempRB.AddForce(targetVector3 * bulletSpeed, ForceMode.Impulse);
+    }
+}
